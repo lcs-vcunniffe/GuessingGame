730c3b71e8a23d89a20af605e7a7d88ae83b622c
Veda Cunniffe <vcunniffe@lcs.on.ca>
2024-01-30 12:02:54 -0500

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by Veda Niav Cunniffe on 2024-01-30.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
