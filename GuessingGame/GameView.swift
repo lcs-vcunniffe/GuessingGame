955bd5bd6d5d28180ac43d53c0b9851f6725f9ac
Veda Cunniffe <vcunniffe@lcs.on.ca>
2024-03-26 12:33:40 -0400

    Made the guessed number the user's input

--- GuessingGame/GameView.swift
+++ GuessingGame/GameView.swift
@@ -12,7 +12,7 @@ struct GameView: View {
     // MARK: Stored properties
     
     // What number the user has guessed
-    @State var selectedNumber = 50
+    @State var givenInput = ""
     
     // What number the computer wants the user to guess
     @State var target = Int.random(in: 1...100)
@@ -34,13 +34,7 @@ struct GameView: View {
                 Text("Guess what it is!")
                     .font(.headline)
                 
-                Stepper(value: $selectedNumber, in: 1...100) {
-                    HStack {
-                        Text("Make a guess:")
-                        Spacer()
-                        Text("\(selectedNumber)")
-                    }
-                }
+                TextField("Make a guess", text: $givenInput)
                 
                 Button {
                     checkGuess()
@@ -93,13 +87,19 @@ struct GameView: View {
     // MARK: Functions
     func checkGuess() {
         
+        //Attempt to unwrap the input provided by the user
+        guard let selectedNumber = Int(givenInput) else {
+            feedback = "Please provide an integer."
+            return
+        }
+        
         // Provide feedback to the user
         if selectedNumber < target {
-            print("Guess higher...")
+            feedback = "Guess higher..."
         } else if selectedNumber > target {
-            print("Guess lower...")
+            feedback = "Guess lower..."
         } else {
-            print("Correct!")
+            feedback = "Correct!"
         }
         
         // Save the user's guesses
@@ -111,7 +111,7 @@ struct GameView: View {
     func reset() {
 
         // Start the user back at 50
-        selectedNumber = 50
+        givenInput = ""
         
         // Have the computer guess a new number
         target = Int.random(in: 1...100)
